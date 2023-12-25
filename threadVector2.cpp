b537e9ad1f51f4f042d0a0812d56b6c186d30cad
kr-saurav <kr.sauravkrsrk@gmail.com>
2023-12-25 21:28:30 +0530

    vector with 10 concurrent and 10 sequential thread

--- threadVector2.cpp
+++ threadVector2.cpp
@@ -2,8 +2,9 @@
 #include <thread>
 #include <vector>
 
-int main(){
 
+
+int main(){
 
     auto lambda = [](int x){
         std::cout << "Hello from thread!" << std::this_thread::get_id() << std::endl;
@@ -11,14 +12,12 @@ int main(){
     };
     std::vector<std::thread> threads;
 
- // here sequentially 10 threads are getting created, if you print 10 thread ID then there will be same IDs all over.
- // same IDs because every iteration 1 thread is getting created and joined and getting terminated and then in next iteration again 1 thread is getting
- // created and so on.
- // so at any time only thread is running.
-
-
     for(int i = 0 ; i < 10 ; i++){
         threads.push_back(std::thread(lambda, i));
+        //threads[i].join();
+    }
+    for(int i = 0 ; i < 10 ; i++){
+            //threads.push_back(std::thread(lambda, i));
         threads[i].join();
     }
 
